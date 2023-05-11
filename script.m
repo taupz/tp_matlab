d043ff04141b1e118f19c911118fc5e7062a02e1
taupz <balthazar.girot@gmail.com>
2023-05-11 14:05:20 +0200

    fix script

--- script.m
+++ script.m
@@ -12,23 +12,20 @@ disp(['zero reel de la fonction : ' num2str(trueValue) newline]);
 [dichotomic_value, nbIterDichotomie, errDichotomie] = dichotomic_func(0, 5, 10^-3, 100, trueValue);
 
 total_time = 0;
-total_error = 0;
 for i = 1:100
     t_start = clock;
-    [a , b , c] = dichotomic_func(0, 5, 10^-6, 100, trueValue);
+    dichotomic_func(0, 5, 10^-3, 100, trueValue);
     t_end = clock;
     exec_time = etime(t_end, t_start);
     total_time = total_time + exec_time;
-    total_error = total_error + c;
 end
 averageTimeDichotomie = total_time/100;
-averageErrDichotomie = total_error/100;
 
 
 disp(['Zero approche par dichotomie : ' num2str(dichotomic_value)]);
+disp(['Erreur  : ' num2str(errDichotomie)]);
 disp(['Nombre d iterations : ' num2str(nbIterDichotomie)]);
-disp(['Temps moyen sur 100 executions : ' num2str(averageTimeDichotomie) 's']);
-disp(['Erreur moyenne sur 100 executions : ' num2str(averageErrDichotomie) newline]);
+disp(['Temps moyen sur 100 executions : ' num2str(averageTimeDichotomie) 's' newline]);
 
 
 
@@ -36,19 +33,16 @@ disp(['Erreur moyenne sur 100 executions : ' num2str(averageErrDichotomie) newli
 [trichotomic_value, nbIterTrichotomie, errTrichotomie]= dichotomic2_func(0, 5, 10^-3, 100, trueValue);
 
 total_time = 0;
-total_error = 0;
 for i = 1:100
     t_start = clock;
-    [a , b , c] = dichotomic2_func(0, 5, 10^-6, 100, trueValue);
+    dichotomic2_func(0, 5, 10^-3, 100, trueValue);
     t_end = clock;
     exec_time = etime(t_end, t_start);
     total_time = total_time + exec_time;
-    total_error = total_error + c;
 end
 averageTimeTrichotomie = total_time/100;
-averageErrTrichotomie = total_error/100;
 
 disp(['Zero approche par trichotomie : ' num2str(trichotomic_value)]);
+disp(['Erreur : ' num2str(errTrichotomie)]);
 disp(['Nombre d iterations : ' num2str(nbIterTrichotomie)]);
-disp(['Temps moyen sur 100 executions : ' num2str(averageTimeTrichotomie) 's']);
-disp(['Erreur moyenne sur 100 executions : ' num2str(averageErrTrichotomie) newline]);
+disp(['Temps moyen sur 100 executions : ' num2str(averageTimeTrichotomie) 's' newline]);
